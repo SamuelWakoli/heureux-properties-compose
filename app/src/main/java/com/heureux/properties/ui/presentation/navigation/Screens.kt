f099e1412592f22452cf4560dc3add4211c82d9d
Sam <swwakoli@gmail.com>
2023-12-19 19:44:19 +0300

    MainScreen bottom bar destinations routes

--- app/src/main/java/com/heureux/properties/ui/presentation/navigation/Screens.kt
+++ app/src/main/java/com/heureux/properties/ui/presentation/navigation/Screens.kt
@@ -3,5 +3,12 @@ package com.heureux.properties.ui.presentation.navigation
 sealed class Screens(val route: String) {
     data object SignInScreen : Screens(route = "sign_in_screen")
     data object RegistrationScreen : Screens(route = "registration_screen")
-    data object HomeScreen : Screens(route = "home_screen")
+    data object MainScreen : Screens(route = "main_screen")
+
+
+    // Bottom Nav
+    data object HomeScreen : Screens(route = "home")
+    data object BookmarksScreen : Screens(route = "bookmarks")
+    data object MyListingsScreen : Screens(route = "my_listings")
+    data object MoreScreen : Screens(route = "more")
 }
