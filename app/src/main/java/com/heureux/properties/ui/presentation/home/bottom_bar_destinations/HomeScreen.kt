f1132b1b5544ac5427ac502ef10884b086256e81
Sam <swwakoli@gmail.com>
2023-12-19 19:43:49 +0300

    MainScreen bottom bar destinations

---
+++ app/src/main/java/com/heureux/properties/ui/presentation/home/bottom_bar_destinations/HomeScreen.kt
@@ -0,0 +1,15 @@
+package com.heureux.properties.ui.presentation.home.bottom_bar_destinations
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HomeScreen() {
+
+}
+
+@Preview
+@Composable
+private fun HomeScreenPreview() {
+    HomeScreen()
+}
