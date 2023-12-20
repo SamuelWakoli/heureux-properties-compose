d4a6b0117f6d5a8ba996ce4ac24eba57b928e491
Sam <swwakoli@gmail.com>
2023-12-20 13:36:36 +0300

    added text at the bottom navigation destinations

---
+++ app/src/main/java/com/heureux/properties/ui/presentation/main/bottom_bar_destinations/BookmarksScreen.kt
@@ -0,0 +1,28 @@
+package com.heureux.properties.ui.presentation.main.bottom_bar_destinations
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun BookmarksScreen() {
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(text = "Bookmarks", style = MaterialTheme.typography.titleMedium)
+    }
+}
+
+@Preview
+@Composable
+private fun BookmarksScreenPreview() {
+    BookmarksScreen()
+}
