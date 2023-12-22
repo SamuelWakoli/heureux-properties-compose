d029bca69cf7c26e007cc4d937690471f10f005b
Sam <swwakoli@gmail.com>
2023-12-22 21:24:54 +0300

    Refined ProfileScreen

--- app/src/main/java/com/heureux/properties/ui/presentation/main/profile_screen/ProfileScreen.kt
+++ app/src/main/java/com/heureux/properties/ui/presentation/main/profile_screen/ProfileScreen.kt
@@ -2,9 +2,11 @@ package com.heureux.properties.ui.presentation.main.profile_screen
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -27,11 +29,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.heureux.properties.ui.presentation.navigation.Screens
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -56,148 +60,163 @@ fun ProfileScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .verticalScroll(state = rememberScrollState())
-                .padding(paddingValues)
+                .verticalScroll(state = rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Text(
-                text = "My details",
-                color = MaterialTheme.colorScheme.primary,
-                modifier = Modifier.padding(horizontal = 16.dp),
-            )
-            ListItem(
-                leadingContent = {
-                    Icon(
-                        imageVector = Icons.Outlined.AccountCircle,
-                        contentDescription = "Profile image",
-                        modifier = Modifier.size(64.dp),
-                    )
-                },
-                headlineContent = {
-                    Text(text = "username")
-                },
-                supportingContent = {
-                    Text(text = "username@gmail.com")
-                },
-            )
-            ListItem(
-                leadingContent = {
-                    Icon(
-                        imageVector = Icons.Outlined.Phone,
-                        contentDescription = null,
-                    )
-                },
-                headlineContent = {
-                    Text(text = "Phone")
-                },
-                supportingContent = {
-                    Text(text = "0712345678")
-                },
-            )
-            Divider(
-                modifier = Modifier.padding(horizontal = 16.dp)
-            )
-            Text(
-                text = "Records",
-                color = MaterialTheme.colorScheme.primary,
+            Column(
                 modifier = Modifier
-                    .padding(horizontal = 16.dp)
-                    .padding(top = 24.dp),
-            )
-            ListItem(
-                leadingContent = {
-                    Icon(
-                        imageVector = Icons.Outlined.Business,
-                        contentDescription = null,
-                    )
-                },
-                headlineContent = {
-                    Text(text = "My properties")
-                },
-                modifier = Modifier.clickable {
-                    // TODO: Navigate to my properties screen
-                }
-            )
-            ListItem(
-                leadingContent = {
-                    Icon(
-                        imageVector = Icons.Outlined.Store,
-                        contentDescription = null,
-                    )
-                },
-                headlineContent = {
-                    Text(text = "Sold properties")
-                },
-                modifier = Modifier.clickable {
-                    // TODO: Navigate to history screen
-                }
-            )
-            ListItem(
-                leadingContent = {
-                    Icon(
-                        imageVector = Icons.Outlined.History,
-                        contentDescription = null,
-                    )
-                },
-                headlineContent = {
-                    Text(text = "Payment history")
-                },
-                modifier = Modifier.clickable {
-                    // TODO: Navigate to history screen
-                }
-            )
-            Divider(
-                modifier = Modifier.padding(horizontal = 16.dp)
-            )
-            Text(
-                text = "Actions",
-                color = MaterialTheme.colorScheme.primary,
-                modifier = Modifier
-                    .padding(horizontal = 16.dp)
-                    .padding(top = 24.dp),
-            )
-            ListItem(
-                leadingContent = {
-                    Icon(
-                        imageVector = Icons.Outlined.Edit,
-                        contentDescription = null,
-                    )
-                },
-                headlineContent = {
-                    Text(text = "Edit profile")
-                },
-                modifier = Modifier.clickable {
-                    // TODO: Navigate to edit profile screen
-                }
-            )
-            ListItem(
-                leadingContent = {
-                    Icon(
-                        imageVector = Icons.Outlined.Logout,
-                        contentDescription = null,
-                    )
-                },
-                headlineContent = {
-                    Text(text = "Sign out")
-                },
-                modifier = Modifier.clickable {
-                    // TODO: Sign out user
-                }
-            )
-            ListItem(
-                leadingContent = {
-                    Icon(
-                        imageVector = Icons.Outlined.NoAccounts,
-                        contentDescription = null,
-                    )
-                },
-                headlineContent = {
-                    Text(text = "Delete profile & data")
-                },
-                modifier = Modifier.clickable {
-                    // TODO: Delete user data and sign out
-                }
-            )
+                    .fillMaxHeight()
+                    .padding(paddingValues)
+                    .widthIn(min = 400.dp, max = 600.dp),
+            ) {
+                Text(
+                    text = "My details",
+                    color = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.padding(horizontal = 16.dp),
+                )
+                ListItem(
+                    leadingContent = {
+                        Icon(
+                            imageVector = Icons.Outlined.AccountCircle,
+                            contentDescription = "Profile image",
+                            modifier = Modifier.size(64.dp),
+                        )
+                    },
+                    headlineContent = {
+                        Text(text = "username")
+                    },
+                    supportingContent = {
+                        Text(text = "username@gmail.com")
+                    },
+                )
+                ListItem(
+                    leadingContent = {
+                        Icon(
+                            imageVector = Icons.Outlined.Phone,
+                            contentDescription = null,
+                        )
+                    },
+                    headlineContent = {
+                        Text(text = "Phone")
+                    },
+                    supportingContent = {
+                        Text(text = "0712345678")
+                    },
+                )
+                Divider(
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                )
+                Text(
+                    text = "Records",
+                    color = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                        .padding(top = 24.dp),
+                )
+                ListItem(
+                    leadingContent = {
+                        Icon(
+                            imageVector = Icons.Outlined.Business,
+                            contentDescription = null,
+                        )
+                    },
+                    headlineContent = {
+                        Text(text = "My properties")
+                    },
+                    modifier = Modifier.clickable {
+                        mainNavController.navigate(route = Screens.MyPropertiesScreen.route) {
+                            launchSingleTop = true
+                        }
+                    }
+                )
+                ListItem(
+                    leadingContent = {
+                        Icon(
+                            imageVector = Icons.Outlined.Store,
+                            contentDescription = null,
+                        )
+                    },
+                    headlineContent = {
+                        Text(text = "Sold properties")
+                    },
+                    modifier = Modifier.clickable {
+                        mainNavController.navigate(route = Screens.SoldPropertiesScreen.route) {
+                            launchSingleTop = true
+                        }
+                    }
+                )
+                ListItem(
+                    leadingContent = {
+                        Icon(
+                            imageVector = Icons.Outlined.History,
+                            contentDescription = null,
+                        )
+                    },
+                    headlineContent = {
+                        Text(text = "Payment history")
+                    },
+                    modifier = Modifier.clickable {
+                        mainNavController.navigate(route = Screens.PaymentHistoryScreen.route) {
+                            launchSingleTop = true
+                        }
+                    }
+                )
+                Divider(
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                )
+                Text(
+                    text = "Actions",
+                    color = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                        .padding(top = 24.dp),
+                )
+                ListItem(
+                    leadingContent = {
+                        Icon(
+                            imageVector = Icons.Outlined.Edit,
+                            contentDescription = null,
+                        )
+                    },
+                    headlineContent = {
+                        Text(text = "Edit profile")
+                    },
+                    modifier = Modifier.clickable {
+                        mainNavController.navigate(route = Screens.EditProfileScreen.route) {
+                            launchSingleTop = true
+                        }
+                    }
+                )
+                ListItem(
+                    leadingContent = {
+                        Icon(
+                            imageVector = Icons.Outlined.Logout,
+                            contentDescription = null,
+                        )
+                    },
+                    headlineContent = {
+                        Text(text = "Sign out")
+                    },
+                    modifier = Modifier.clickable {
+                        // TODO: Sign out user
+                    }
+                )
+                ListItem(
+                    leadingContent = {
+                        Icon(
+                            imageVector = Icons.Outlined.NoAccounts,
+                            contentDescription = null,
+                        )
+                    },
+                    headlineContent = {
+                        Text(text = "Delete profile & data")
+                    },
+                    modifier = Modifier.clickable {
+                        // TODO: Delete user data and sign out
+                    }
+                )
 
+            }
         }
     }
 }
