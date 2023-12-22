01072fa043078f19197d48b58e6fd5ac965a921f
Sam <swwakoli@gmail.com>
2023-12-22 12:39:47 +0300

    defined theme colors

--- app/src/main/java/com/heureux/properties/ui/theme/Color.kt
+++ app/src/main/java/com/heureux/properties/ui/theme/Color.kt
@@ -2,10 +2,34 @@ package com.heureux.properties.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Light Theme
+val primaryColorLight = Color(0xFF785A00)
+val onPrimaryColorLight = Color(0xFFFFFFFF)
+val primaryContainerColorLight = Color(0xFFFFDF9D)
+val onPrimaryContainerColorLight = Color(0xFF251A00)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val secondaryColorLight = Color(0xFF785A00)
+val onSecondaryColorLight = Color(0xFFFFFFFF)
+val secondaryContainerColorLight = Color(0xFFFFDF9D)
+val onSecondaryContainerColorLight = Color(0xFF251A00)
+
+val tertiaryColorLight = Color(0xFF006E1C)
+val onTertiaryColorLight = Color(0xFFFFFFFF)
+val tertiaryContainerColorLight = Color(0xFF94F990)
+val onTertiaryContainerColorLight = Color(0xFF002204)
+
+// Dark Theme
+val primaryColorDark = Color(0xFFF4BF3D)
+val onPrimaryColorDark = Color(0xFF3F2E00)
+val primaryContainerColorDark = Color(0xFF5B4300)
+val onPrimaryContainerColorDark = Color(0xFFFFDF9D)
+
+val secondaryColorDark = Color(0xFF9ECAFF)
+val onSecondaryColorDark = Color(0xFF003258)
+val secondaryContainerColorDark = Color(0xFF00497D)
+val onSecondaryContainerColorDark = Color(0xFFD1E4FF)
+
+val tertiaryColorDark = Color(0xFF78DC77)
+val onTertiaryColorDark = Color(0xFF00390A)
+val tertiaryContainerColorDark = Color(0xFF005313)
+val onTertiaryContainerColorDark = Color(0xFF94F990)
