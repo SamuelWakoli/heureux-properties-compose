225f4aa3c3afa0bf34c48acd8aac61ec9f3e1b49
Sam <swwakoli@gmail.com>
2023-12-23 16:18:56 +0300

    Authentication logic

--- app/src/main/java/com/heureux/properties/ui/presentation/viewmodels/AuthViewModel.kt
+++ app/src/main/java/com/heureux/properties/ui/presentation/viewmodels/AuthViewModel.kt
@@ -1,14 +1,17 @@
 package com.heureux.properties.ui.presentation.viewmodels
 
 import androidx.lifecycle.ViewModel
-import com.heureux.properties.data.AuthRepository
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.auth.ktx.userProfileChangeRequest
+import com.google.firebase.ktx.Firebase
+import com.heureux.properties.data.FirestoreRepository
 import com.heureux.properties.ui.presentation.authgate.GoogleSignInResult
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
-data class SignInUiModel(
+data class SignInUiState(
     // userData
     val name: String = "",
     val email: String = "",
@@ -28,9 +31,9 @@ data class SignInUiModel(
     val showDialogPwdResetEmailSent: Boolean = false
 )
 
-class AuthViewModel(userAuthRepository: AuthRepository) : ViewModel() {
-    private var _uiState: MutableStateFlow<SignInUiModel> = MutableStateFlow(SignInUiModel())
-    val uiState: StateFlow<SignInUiModel> = _uiState.asStateFlow()
+class AuthViewModel(heureuxFirestoreRepository: FirestoreRepository) : ViewModel() {
+    private var _uiState: MutableStateFlow<SignInUiState> = MutableStateFlow(SignInUiState())
+    val uiState: StateFlow<SignInUiState> = _uiState.asStateFlow()
 
     fun onSignInResult(result: GoogleSignInResult) {
         _uiState.update {
@@ -39,5 +42,167 @@ class AuthViewModel(userAuthRepository: AuthRepository) : ViewModel() {
                 errorMessage = result.errorMessage,
             )
         }
+    }
+
+    fun updateName(value: String) {
+        _uiState.update {
+            it.copy(
+                name = value,
+                showNameError = false,
+                errorMessage = null,
+            )
+        }
+    }
+
+
+    fun updateEmail(value: String) {
+        _uiState.update {
+            it.copy(
+                email = value,
+                showEmailError = false,
+                errorMessage = null,
+            )
+        }
+    }
+
+    fun updatePassword(value: String) {
+
+        _uiState.update {
+            it.copy(
+                password = value,
+                showPasswordError = false,
+                errorMessage = null
+            )
+        }
+    }
+
+    fun hideOrShowPassword() {
+        _uiState.update { it.copy(showPassword = !(it.showPassword)) }
+    }
+
+    fun signInWithEmailPwd() {
+        val email = uiState.value.email
+        val password = uiState.value.password
+        if (email.isEmpty()) {
+            _uiState.update { it.copy(showEmailError = true) }
+        } else if (password.isEmpty()) {
+            _uiState.update { it.copy(showPasswordError = true) }
+        } else {
+            _uiState.update {
+                it.copy(
+                    isSignInButtonLoading = true
+                )
+            }
+
+            Firebase.auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { authResultTask ->
+                    if (authResultTask.isSuccessful) {
+                        _uiState.update {
+                            it.copy(
+                                isSignInSuccess = true,
+                            )
+                        }
+                    }
+                    if (authResultTask.isCanceled) {
+                        _uiState.update {
+                            it.copy(
+                                isSignInButtonLoading = false,
+                                errorMessage = "Sign in canceled"
+                            )
+                        }
+                    }
+                }.addOnFailureListener { exception ->
+                    _uiState.update {
+                        it.copy(
+                            isSignInButtonLoading = false,
+                            errorMessage = exception.message
+                        )
+                    }
+                }
+        }
+    }
+
+    fun registerWithEmailPwd() {
+        val name = uiState.value.name
+        val email = uiState.value.email
+        val password = uiState.value.password
+
+        if (name.isEmpty()) {
+            _uiState.update { it.copy(showNameError = true) }
+        } else if (email.isEmpty()) {
+            _uiState.update { it.copy(showEmailError = true) }
+        } else if (password.isEmpty()) {
+            _uiState.update { it.copy(showPasswordError = true) }
+        } else {
+            _uiState.update {
+                it.copy(
+                    isCreateAccountButtonLoading = true
+                )
+            }
+
+            Firebase.auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { authResultTask ->
+                    if (authResultTask.isSuccessful) {
+                        // since user registration is successful, update display name
+                        val profileUpdate = userProfileChangeRequest {
+                            displayName = name
+                        }
+                        Firebase.auth.currentUser?.updateProfile(profileUpdate)
+
+                        _uiState.update {
+                            it.copy(
+                                isSignInSuccess = true,
+                            )
+                        }
+                    }
+                    if (authResultTask.isCanceled) {
+                        _uiState.update {
+                            it.copy(
+                                isCreateAccountButtonLoading = false,
+                                errorMessage = "Registration canceled",
+                            )
+                        }
+                    }
+                }.addOnFailureListener { exception ->
+                    _uiState.update {
+                        it.copy(
+                            isCreateAccountButtonLoading = false,
+                            errorMessage = exception.message
+                        )
+                    }
+                }
+
+        }
+    }
+
+    fun sendPasswordResetEmail() {
+        val email = uiState.value.email
+        if (email.isEmpty()) {
+            _uiState.update { it.copy(showEmailError = true) }
+        } else {
+            _uiState.update {
+                it.copy(
+                    isSignInButtonLoading = true
+                )
+            }
+        }
+
+        Firebase.auth.sendPasswordResetEmail(email).addOnSuccessListener {
+            _uiState.update {
+                it.copy(
+                    isSignInButtonLoading = !it.isSignInButtonLoading,
+                    showDialogPwdResetEmailSent = !it.showDialogPwdResetEmailSent,
+                )
+            }
+        }.addOnFailureListener { error ->
+            _uiState.update {
+                it.copy(
+                    isSignInButtonLoading = false,
+                    showDialogPwdResetEmailSent = !it.showDialogPwdResetEmailSent,
+                    errorMessage = error.message
+                )
+            }
+        }
+
     }
 }
