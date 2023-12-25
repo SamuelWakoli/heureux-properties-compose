bf4738e70e53979d9631db840a730c4775fe284d
Sam <swwakoli@gmail.com>
2023-12-25 15:34:01 +0300

    created userData: StateFlow getting Flow data from FirestoreRepository

--- app/src/main/java/com/heureux/properties/ui/presentation/viewmodels/MainScreenViewModel.kt
+++ app/src/main/java/com/heureux/properties/ui/presentation/viewmodels/MainScreenViewModel.kt
@@ -1,8 +1,35 @@
 package com.heureux.properties.ui.presentation.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.heureux.properties.data.FirestoreRepository
+import com.heureux.properties.data.HeureuxUser
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
 
 class MainScreenViewModel(heureuxFirestoreRepository: FirestoreRepository) : ViewModel() {
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    private val currentUser = Firebase.auth.currentUser!!
+
+    val userData: StateFlow<HeureuxUser?> = heureuxFirestoreRepository.getHeureuxUserData(
+        user = currentUser,
+        onSuccess = {
+
+        },
+        onFailure = { exception: Exception -> }
+    ).stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+            initialValue = null
+        )
+
+
 
 }
