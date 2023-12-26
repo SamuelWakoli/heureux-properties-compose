d97507f3712e634a82f5295fcb40838897770ddd
Sam <swwakoli@gmail.com>
2023-12-26 09:13:06 +0300

    added icons at the appbar titles

--- app/src/main/java/com/heureux/properties/ui/presentation/main/sold_properties_screen/SoldPropertiesScreen.kt
+++ app/src/main/java/com/heureux/properties/ui/presentation/main/sold_properties_screen/SoldPropertiesScreen.kt
@@ -1,10 +1,15 @@
 package com.heureux.properties.ui.presentation.main.sold_properties_screen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.outlined.Store
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -12,8 +17,10 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 
@@ -33,7 +40,17 @@ fun SoldPropertiesScreen(
                 }
             },
             title = {
-                Text(text = "Sold Properties")
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Icon(
+                        imageVector = Icons.Outlined.Store,
+                        contentDescription = null,
+                    )
+                    Spacer(modifier = Modifier.size(8.dp))
+                    Text(text = "Sold Properties")
+                }
             },
         )
     }) { paddingValues ->
