3746ea93d62c19736cedcdfd94eef081b3c8d2ca
Sam <swwakoli@gmail.com>
2023-12-26 22:17:52 +0300

    SoldPropertyListItem design

--- app/src/main/java/com/heureux/properties/ui/presentation/main/sold_properties_screen/SoldPropertiesScreen.kt
+++ app/src/main/java/com/heureux/properties/ui/presentation/main/sold_properties_screen/SoldPropertiesScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.outlined.Store
@@ -21,18 +22,23 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.heureux.properties.ui.presentation.composables.property_list_item.SoldPropertyListItem
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SoldPropertiesScreen(
     mainNavController: NavController,
 ) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
     Scaffold(topBar = {
         CenterAlignedTopAppBar(
+            scrollBehavior = scrollBehavior,
             navigationIcon = {
                 IconButton(onClick = { mainNavController.navigateUp() }) {
                     Icon(
@@ -61,10 +67,20 @@ fun SoldPropertiesScreen(
     }) { paddingValues ->
         Column(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
+                .fillMaxSize().padding(paddingValues)
+                .nestedScroll(scrollBehavior.nestedScrollConnection),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-
+            LazyColumn(
+                Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                items(20) {
+                    SoldPropertyListItem()
+                }
+            }
         }
     }
 }
