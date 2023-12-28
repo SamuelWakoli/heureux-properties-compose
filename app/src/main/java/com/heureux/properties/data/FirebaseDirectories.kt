0e7dc37c96cd2bc202eb51752bb6463994f47d62
Sam <swwakoli@gmail.com>
2023-12-28 13:39:58 +0300

    Added more field at the FirebaseDirectories

--- app/src/main/java/com/heureux/properties/data/FirebaseDirectories.kt
+++ app/src/main/java/com/heureux/properties/data/FirebaseDirectories.kt
@@ -2,4 +2,14 @@ package com.heureux.properties.data
 
 sealed class FirebaseDirectories (val name: String) {
     data object UsersCollection : FirebaseDirectories(name = "users")
+    data object UsersStorageReference : FirebaseDirectories(name = "users")
+}
+
+sealed class FireStoreUserFields (val field: String) {
+    data object PhotoUrl:FireStoreUserFields ("photoUrl")
+    data object Name:FireStoreUserFields("name")
+    data object Phone:FireStoreUserFields("phone")
+    data object Bookmarks:FireStoreUserFields("bookmarks")
+    data object PropertiesOwned:FireStoreUserFields("propertiesOwned")
+    data object Listings: FireStoreUserFields("listings")
 }
