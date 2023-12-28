9d68950ee5811b6502ca489a09ab88c21ffd0c0a
Sam <swwakoli@gmail.com>
2023-12-28 13:49:12 +0300

    Created ProfileRepository

---
+++ app/src/main/java/com/heureux/properties/data/repositories/ProfileRepository.kt
@@ -0,0 +1,55 @@
+package com.heureux.properties.data.repositories
+
+import com.google.firebase.auth.FirebaseUser
+import com.heureux.properties.data.types.UserProfileData
+import kotlinx.coroutines.flow.Flow
+
+interface ProfileRepository {
+    suspend fun registerUser(
+        name: String,
+        email: String,
+        password: String,
+        onSuccessListener: () -> Unit,
+        onErrorListener: (exception: Exception) -> Unit,
+    )
+
+    suspend fun signIn(
+        name: String,
+        email: String,
+        password: String,
+        onSuccessListener: () -> Unit,
+        onErrorListener: (exception: Exception) -> Unit,
+    )
+
+    fun getUserProfileData(
+        user: FirebaseUser,
+        onSuccess: () -> Unit,
+        onFailure: (exception: Exception) -> Unit,
+    ): Flow<UserProfileData?>
+
+    suspend fun sendPasswordResetEmail(
+        userProfileDate: UserProfileData,
+        onSuccess: () -> Unit,
+        onFailure: (exception: Exception) -> Unit,
+    )
+
+    suspend fun updateUserProfile(
+        userProfileDate: UserProfileData,
+        onSuccess: () -> Unit,
+        onFailure: (exception: Exception) -> Unit,
+    )
+
+    suspend fun createUserFirestoreData(
+        user: FirebaseUser,
+        onSuccess: () -> Unit,
+        onFailure: (exception: Exception) -> Unit,
+    )
+
+    suspend fun signOut()
+
+    suspend fun deleteUserAndData(
+        userProfileDate: UserProfileData,
+        onSuccessListener: () -> Unit,
+        onErrorListener: (exception: Exception) -> Unit,
+    )
+}
