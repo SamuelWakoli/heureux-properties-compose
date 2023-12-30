ef6173f42cc26b9fb008529191a136b9182aac27
Sam <swwakoli@gmail.com>
2023-12-30 14:45:00 +0300

    Added layout for EditPropertyScreen

---
+++ app/src/main/java/com/heureux/properties/ui/presentation/main/property_action_screens/EditPropertyScreen.kt
@@ -0,0 +1,64 @@
+package com.heureux.properties.ui.presentation.main.property_action_screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditPropertyScreen() {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            imageVector = Icons.Outlined.Edit,
+                            contentDescription = null
+                        )
+                        Spacer(modifier = Modifier.padding(8.dp))
+                        Text(text = "Edit Property")
+                    }
+                }, navigationIcon = {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "Navigate back"
+                    )
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                )
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            Modifier.padding(paddingValues)
+        ) {
+
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun EditPropertyScreenPreview() {
+    EditPropertyScreen()
+}
