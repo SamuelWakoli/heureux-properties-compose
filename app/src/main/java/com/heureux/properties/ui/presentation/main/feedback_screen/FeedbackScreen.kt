ba181cfc298386934ef6d762bcef9d8b1b4e960e
Sam <swwakoli@gmail.com>
2024-01-01 21:37:10 +0300

    FeedbackScreen design

--- app/src/main/java/com/heureux/properties/ui/presentation/main/feedback_screen/FeedbackScreen.kt
+++ app/src/main/java/com/heureux/properties/ui/presentation/main/feedback_screen/FeedbackScreen.kt
@@ -1,23 +1,62 @@
 package com.heureux.properties.ui.presentation.main.feedback_screen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.outlined.Feedback
+import androidx.compose.material.icons.outlined.Send
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FeedbackScreen(navController: NavHostController) {
+
+    var feedbackText by rememberSaveable {
+        mutableStateOf("")
+    }
+
+    var showFeedbackError by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    val focusRequester = FocusRequester()
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
+
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
@@ -29,15 +68,67 @@ fun FeedbackScreen(navController: NavHostController) {
                         )
                     }
                 },
-                title = { Text(text = "Feedback") })
+                title = {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(imageVector = Icons.Outlined.Feedback, contentDescription = null)
+                        Spacer(modifier = Modifier.size(8.dp))
+                        Text(text = "Feedback")
+                    }
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                ),
+            )
         }
     ) { paddingValues ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(paddingValues)
+                .padding(paddingValues),
+            verticalArrangement = Arrangement.SpaceBetween
         ) {
-
+            OutlinedTextField(
+                value = feedbackText,
+                onValueChange = { value ->
+                    feedbackText = value
+                },
+                modifier = Modifier
+                    .widthIn(max = 600.dp)
+                    .weight(1f)
+                    .focusRequester(focusRequester),
+                placeholder = {
+                    Text(text = "Type here")
+                },
+                supportingText = {
+                    if (showFeedbackError) Text(text = "Cannot send empty message")
+                },
+                isError = showFeedbackError,
+                keyboardOptions = KeyboardOptions(
+                    capitalization = KeyboardCapitalization.Sentences,
+                    autoCorrect = true,
+                ),
+                singleLine = false,
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = Color.Transparent,
+                    unfocusedBorderColor = Color.Transparent,
+                )
+            )
+            ElevatedButton(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.align(Alignment.CenterHorizontally).padding(16.dp)
+            ) {
+                Row(
+                    modifier = Modifier.widthIn(min = 400.dp, max = 600.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center,
+                ) {
+                    Text(text = "Send", style = MaterialTheme.typography.titleMedium)
+                    Spacer(modifier = Modifier.size(8.dp))
+                    Icon(imageVector = Icons.Outlined.Send, contentDescription = null)
+                }
+            }
         }
     }
 }
