5056460f254e60e4989a1d971ce254df85319458
Sam <swwakoli@gmail.com>
2024-01-02 15:59:41 +0300

    Definition of PropertiesDataSource

--- app/src/main/java/com/heureux/properties/data/sources/PropertiesDataSource.kt
+++ app/src/main/java/com/heureux/properties/data/sources/PropertiesDataSource.kt
@@ -1,10 +1,12 @@
 package com.heureux.properties.data.sources
 
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
-import com.heureux.properties.data.types.UserProfileData
+import com.heureux.properties.data.types.HeureuxProperty
+import com.heureux.properties.data.types.InquiryItem
+import com.heureux.properties.data.types.NotificationItem
+import com.heureux.properties.data.types.PaymentItem
 import kotlinx.coroutines.flow.Flow
 
 interface PropertiesDataSource {
@@ -12,11 +14,65 @@ interface PropertiesDataSource {
     val firestore: FirebaseFirestore
     val storage: FirebaseStorage
 
-    fun getUserProfileData(
-        user: FirebaseUser,
+
+    fun getHomeProperties(
         onSuccess: () -> Unit,
         onFailure: (exception: Exception) -> Unit,
-    ): Flow<UserProfileData?>
+    ): Flow<List<HeureuxProperty>>
 
+
+    fun getMyProperties(
+        email: String,
+        onSuccess: () -> Unit,
+        onFailure: (exception: Exception) -> Unit,
+    ): Flow<List<HeureuxProperty>>
+
+
+    fun getSoldProperties(
+        email: String,
+        onSuccess: () -> Unit,
+        onFailure: (exception: Exception) -> Unit,
+    ): Flow<List<HeureuxProperty>>
+
+
+    fun getPaymentHistory(
+        email: String,
+        onSuccess: () -> Unit,
+        onFailure: (exception: Exception) -> Unit,
+    ): Flow<List<PaymentItem>>
+
+
+    fun getBookmarks(
+        email: String,
+        onSuccess: () -> Unit,
+        onFailure: (exception: Exception) -> Unit,
+    ): Flow<List<HeureuxProperty>>
+
+
+    fun getMyListings(
+        email: String,
+        onSuccess: () -> Unit,
+        onFailure: (exception: Exception) -> Unit,
+    ): Flow<HeureuxProperty>
+
+    fun getNotifications(
+        email: String,
+        onSuccess: () -> Unit,
+        onFailure: (exception: Exception) -> Unit,
+    ): Flow<List<NotificationItem>>
+
+
+    fun getMyInquires(
+        email: String,
+        onSuccess: () -> Unit,
+        onFailure: (exception: Exception) -> Unit,
+    ): Flow<List<InquiryItem>>
+
+
+    fun getPropertyItem(
+        email: String,
+        onSuccess: () -> Unit,
+        onFailure: (exception: Exception) -> Unit,
+    ): Flow<HeureuxProperty>
 
 }
