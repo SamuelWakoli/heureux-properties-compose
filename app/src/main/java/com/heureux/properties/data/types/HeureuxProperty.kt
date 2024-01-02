e138f76121e7ef747351de1c2cf11040d89b29e3
Sam <swwakoli@gmail.com>
2024-01-02 22:59:31 +0300

    Added more fields for HeureuxProperty

--- app/src/main/java/com/heureux/properties/data/types/HeureuxProperty.kt
+++ app/src/main/java/com/heureux/properties/data/types/HeureuxProperty.kt
@@ -1,10 +1,13 @@
 package com.heureux.properties.data.types
 
 data class HeureuxProperty(
+    val id: String,
     val name: String,
     val price: String,
     val location: String,
     val seller: String, // used to determine user listings
     val description: String,
     val imageUrls: List<String>,
+    val sellerCode: String, // used to determine user listings
+    val purchasedBy: String? = null, // used to determine if listing is sold
 )
