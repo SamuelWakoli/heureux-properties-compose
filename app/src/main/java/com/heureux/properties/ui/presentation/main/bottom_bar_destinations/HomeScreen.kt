de9590b4db395dd9f8ac970b1942eeff97d08912
Sam <swwakoli@gmail.com>
2024-01-03 21:12:17 +0300

    Empty property screen design

--- app/src/main/java/com/heureux/properties/ui/presentation/main/bottom_bar_destinations/HomeScreen.kt
+++ app/src/main/java/com/heureux/properties/ui/presentation/main/bottom_bar_destinations/HomeScreen.kt
@@ -2,16 +2,28 @@ package com.heureux.properties.ui.presentation.main.bottom_bar_destinations
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Business
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.heureux.properties.ui.presentation.composables.property_list_item.PropertyListItem
@@ -22,6 +34,9 @@ fun HomeScreen(
     navController: NavController,
     scrollBehavior: TopAppBarScrollBehavior,
 ) {
+
+    var isItemsEmpty by remember { mutableStateOf(false) }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -29,15 +44,34 @@ fun HomeScreen(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        LazyColumn(
-            Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            items(20) {
-                PropertyListItem(
-                    navController = navController
+        if (!isItemsEmpty) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.Business,
+                    contentDescription = null,
+                    modifier = Modifier.size(128.dp),
+                    tint = MaterialTheme.colorScheme.primary,
                 )
+                Spacer(modifier = Modifier.size(16.dp))
+                Text(
+                    text = "No Properties Found",
+                    style = MaterialTheme.typography.titleLarge,
+                    color = MaterialTheme.colorScheme.primary,
+                )
+            }
+        } else {
+            LazyColumn(
+                Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                items(20) {
+                    PropertyListItem(
+                        navController = navController
+                    )
+                }
             }
         }
     }
