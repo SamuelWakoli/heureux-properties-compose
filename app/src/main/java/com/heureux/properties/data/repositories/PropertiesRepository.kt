a7f18d3d646ab7250c96057d8a2530594d49eb55
Sam <swwakoli@gmail.com>
2024-01-03 20:22:27 +0300

    PropertiesRepository design

---
+++ app/src/main/java/com/heureux/properties/data/repositories/PropertiesRepository.kt
@@ -0,0 +1,65 @@
+package com.heureux.properties.data.repositories
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.heureux.properties.data.types.HeureuxProperty
+import com.heureux.properties.data.types.InquiryItem
+import com.heureux.properties.data.types.NotificationItem
+import com.heureux.properties.data.types.PaymentItem
+import kotlinx.coroutines.flow.Flow
+
+interface PropertiesRepository {
+
+    val firestore: FirebaseFirestore
+
+
+    fun getHomeProperties(
+        onFailure: (exception: Exception) -> Unit,
+    ): Flow<List<HeureuxProperty>>
+
+
+    fun getMyProperties(
+        email: String,
+        onFailure: (exception: Exception) -> Unit,
+    ): Flow<List<HeureuxProperty>>
+
+
+    fun getUserSoldProperties(
+        email: String,
+        onFailure: (exception: Exception) -> Unit,
+    ): Flow<List<HeureuxProperty>>
+
+
+    fun getPaymentHistory(
+        email: String,
+        onFailure: (exception: Exception) -> Unit,
+    ): Flow<List<PaymentItem>>
+
+
+    fun getBookmarks(
+        email: String,
+        onFailure: (exception: Exception) -> Unit,
+    ): Flow<List<HeureuxProperty>>
+
+
+    fun getMyListings(
+        email: String,
+        onFailure: (exception: Exception) -> Unit,
+    ): Flow<List<HeureuxProperty>>
+
+
+    fun getNotifications(
+        email: String,
+        onFailure: (exception: Exception) -> Unit,
+    ): Flow<List<NotificationItem>>
+
+
+    fun getMyInquires(
+        email: String,
+        onFailure: (exception: Exception) -> Unit,
+    ): Flow<List<InquiryItem>>
+
+
+    fun getPropertyItem(
+        propertyId: String,
+    ): Flow<HeureuxProperty>
+}
