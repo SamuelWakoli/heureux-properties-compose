988ff0d8581fa7f0007c4f0c2b9fbd6af529618b
Sam <swwakoli@gmail.com>
2024-01-04 18:43:07 +0300

    Added PropertiesRepository to the AppContainer

--- app/src/main/java/com/heureux/properties/data/AppContainer.kt
+++ app/src/main/java/com/heureux/properties/data/AppContainer.kt
@@ -2,15 +2,22 @@ package com.heureux.properties.data
 
 import android.content.Context
 import com.heureux.properties.data.repositories.HeureuxProfileRepository
+import com.heureux.properties.data.repositories.HeureuxPropertiesRepository
 import com.heureux.properties.data.repositories.ProfileRepository
+import com.heureux.properties.data.repositories.PropertiesRepository
 import com.heureux.properties.data.sources.HeureuxProfileDataSource
+import com.heureux.properties.data.sources.HeureuxPropertiesDataSource
 
 interface AppContainer {
     val profileRepository: ProfileRepository
+    val propertiesRepository: PropertiesRepository
 }
 
 class HeureuxAppContainer(context: Context) : AppContainer {
 
     override val profileRepository: ProfileRepository =
         HeureuxProfileRepository(dataSource = HeureuxProfileDataSource())
+
+    override val propertiesRepository: PropertiesRepository =
+        HeureuxPropertiesRepository(dataSource = HeureuxPropertiesDataSource())
 }
