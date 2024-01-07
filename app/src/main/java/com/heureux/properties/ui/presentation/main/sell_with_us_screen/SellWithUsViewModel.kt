b07da76633a03e9c702a698e4c3d6e931e9ee8a1
Sam <swwakoli@gmail.com>
2024-01-07 11:05:10 +0300

    Created SellWithUsScreen state handling logic

--- app/src/main/java/com/heureux/properties/ui/presentation/main/sell_with_us_screen/SellWithUsViewModel.kt
+++ app/src/main/java/com/heureux/properties/ui/presentation/main/sell_with_us_screen/SellWithUsViewModel.kt
@@ -19,12 +19,13 @@ class SellWithUsViewModel(
     private var _uiState = MutableStateFlow(SellWithUsUiState())
     val uiState = _uiState.asStateFlow()
 
+
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
     }
 
-    val userProfileData = profileRepository.getUserProfileData(onSuccess = {}, onFailure = {})
-        .stateIn(
+    val userProfileData =
+        profileRepository.getUserProfileData(onSuccess = {}, onFailure = {}).stateIn(
             scope = viewModelScope,
             started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
             initialValue = null
@@ -34,14 +35,54 @@ class SellWithUsViewModel(
         sellWithUsRequest: SellWithUsRequest,
         onSuccessListener: () -> Unit,
         onFailure: (exception: Exception) -> Unit,
-    ) = viewModelScope.launch {
-        propertiesRepository.sendSellWithUsRequest(
-            sellWithUsRequest = sellWithUsRequest,
-            onSuccessListener = onSuccessListener,
-            onFailure = onFailure
-        )
+    ) {
+        if (uiState.value.propertyName.isEmpty()) {
+            _uiState.value = _uiState.value.copy(propertyNameError = true)
+        } else if (uiState.value.propertyDescription.isEmpty()) {
+            _uiState.value = _uiState.value.copy(propertyDescriptionError = true)
+        } else if (uiState.value.propertyPrice.isEmpty()) {
+            _uiState.value = _uiState.value.copy(propertyPriceError = true)
+        } else if (uiState.value.userPhoneNumber.isEmpty()) {
+            _uiState.value = _uiState.value.copy(userPhoneNumberError = true)
+        } else if (uiState.value.propertiesImagesCount == 0) {
+            _uiState.value = _uiState.value.copy(propertyImagesError = true)
+        } else {
+
+            viewModelScope.launch {
+                propertiesRepository.sendSellWithUsRequest(
+                    sellWithUsRequest = sellWithUsRequest,
+                    onSuccessListener = onSuccessListener,
+                    onFailure = onFailure
+                )
+            }
+        }
     }
 
+    fun loadUserPhoneNumber() {
+        _uiState.value = _uiState.value.copy(userPhoneNumber = userProfileData.value?.phone ?: "")
+    }
+
+    fun onPropertyNameChanged(name: String) {
+        _uiState.value = _uiState.value.copy(propertyName = name, propertyNameError = false)
+    }
+
+
+    fun onPropertyDescriptionChanged(description: String) {
+        _uiState.value =
+            _uiState.value.copy(propertyDescription = description, propertyDescriptionError = false)
+    }
+
+    fun omPropertyPriceChanged(price: String) {
+        _uiState.value = _uiState.value.copy(propertyPrice = price, propertyPriceError = false)
+    }
+
+    fun onPhoneNumberChanged(phone: String) {
+        _uiState.value = _uiState.value.copy(userPhoneNumber = phone, userPhoneNumberError = false)
+    }
+
+    fun uploadImage() {
+        // TODO:  
+    }
 
 
 }
