def7a0f0f0bba199c27518872348b4452fac5840
Sam <swwakoli@gmail.com>
2024-01-07 11:04:17 +0300

    added more variables at the SellWithUsUiState

--- app/src/main/java/com/heureux/properties/ui/presentation/main/sell_with_us_screen/SellWithUsUiState.kt
+++ app/src/main/java/com/heureux/properties/ui/presentation/main/sell_with_us_screen/SellWithUsUiState.kt
@@ -2,8 +2,15 @@ package com.heureux.properties.ui.presentation.main.sell_with_us_screen
 
 data class SellWithUsUiState(
     val propertyName: String = "",
+    val propertyNameError: Boolean = false,
     val propertyDescription: String = "",
+    val propertyDescriptionError: Boolean = false,
     val propertyPrice: String = "",
+    val propertyPriceError: Boolean = false,
+    val propertiesImagesCount: Int = 0,
     val propertyImages: List<String> = emptyList(),
+    val propertyImagesError: Boolean= false,
     val userPhoneNumber: String = "",
+    val userPhoneNumberError: Boolean = false,
+    val uploadingRequest: Boolean = false,
 )
