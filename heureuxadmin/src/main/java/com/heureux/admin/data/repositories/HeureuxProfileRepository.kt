49a383e0e39dc62094bc836ddb4988c4f12bd661
Sam <swwakoli@gmail.com>
2024-01-08 11:15:33 +0300

    inserted ProfileRepository

---
+++ heureuxadmin/src/main/java/com/heureux/admin/data/repositories/HeureuxProfileRepository.kt
@@ -0,0 +1,116 @@
+package com.heureux.admin.data.repositories
+
+import android.net.Uri
+import com.google.firebase.auth.FirebaseUser
+import com.heureux.admin.data.sources.ProfileDataSource
+import com.heureux.admin.data.types.UserProfileData
+import kotlinx.coroutines.flow.Flow
+
+class HeureuxProfileRepository(val dataSource: ProfileDataSource) : ProfileRepository {
+
+    override suspend fun uploadImageGetUrl(
+        uri: Uri,
+        directory: String,
+        onSuccessListener: (downloadUrl: String) -> Unit,
+        onFailure: (exception: Exception) -> Unit,
+    ) = dataSource.uploadImageGetUrl(
+        uri, directory, onSuccessListener, onFailure
+    )
+
+    override suspend fun registerUser(
+        name: String,
+        email: String,
+        password: String,
+        onSuccessListener: () -> Unit,
+        onErrorListener: (exception: Exception) -> Unit,
+    ) {
+        dataSource.registerUser(
+            name = name,
+            email = email,
+            password = password,
+            onSuccessListener = onSuccessListener,
+            onErrorListener = onErrorListener
+        )
+    }
+
+    override suspend fun signIn(
+        email: String,
+        password: String,
+        onSuccessListener: () -> Unit,
+        onErrorListener: (exception: Exception) -> Unit,
+    ) {
+        dataSource.signIn(
+            email = email,
+            password = password,
+            onSuccessListener = onSuccessListener,
+            onErrorListener = onErrorListener
+        )
+    }
+
+    override fun getUserProfileData(
+        onSuccess: () -> Unit,
+        onFailure: (exception: Exception) -> Unit,
+    ): Flow<UserProfileData?> {
+        return dataSource.getUserProfileData(
+            onSuccess = onSuccess,
+            onFailure = onFailure
+        )
+    }
+
+    override suspend fun sendPasswordResetEmail(
+        email: String,
+        onSuccess: () -> Unit,
+        onFailure: (exception: Exception) -> Unit,
+    ) {
+        dataSource.sendPasswordResetEmail(
+            email = email,
+            onSuccess = onSuccess,
+            onFailure = onFailure
+        )
+    }
+
+    override suspend fun updateUserProfile(
+        userProfileData: UserProfileData,
+        onSuccess: () -> Unit,
+        onFailure: (exception: Exception) -> Unit,
+    ) {
+        dataSource.updateUserProfile(
+            userProfileDate = userProfileData,
+            onSuccess = onSuccess,
+            onFailure = onFailure
+        )
+    }
+
+    override suspend fun createUserFirestoreData(
+        user: UserProfileData,
+        onSuccess: () -> Unit,
+        onFailure: (exception: Exception) -> Unit,
+    ) {
+        dataSource.createUserFirestoreData(
+            user = user,
+            onSuccess = onSuccess,
+            onFailure = onFailure
+        )
+    }
+
+    override suspend fun signOut() {
+        dataSource.signOut()
+    }
+
+    override suspend fun deleteUserAndData(
+        email: String,
+        onSuccessListener: () -> Unit,
+        onErrorListener: (exception: Exception) -> Unit,
+    ) {
+        dataSource.deleteUserAndData(
+            email = email,
+            onSuccessListener = onSuccessListener,
+            onErrorListener = onErrorListener
+        )
+    }
+
+
+    override suspend fun getCurrentUser(): Flow<FirebaseUser?> =
+        dataSource.getCurrentUser()
+
+}
---
+++ heureuxadmin/src/main/java/com/heureux/admin/data/repositories/ProfileRepository.kt
@@ -0,0 +1,64 @@
+package com.heureux.admin.data.repositories
+
+import android.net.Uri
+import com.google.firebase.auth.FirebaseUser
+import com.heureux.admin.data.types.UserProfileData
+import kotlinx.coroutines.flow.Flow
+
+interface ProfileRepository {
+
+    suspend fun uploadImageGetUrl(
+        uri: Uri,
+        directory:String,
+        onSuccessListener: (downloadUrl: String) -> Unit,
+        onFailure: (exception: Exception) -> Unit
+    )
+
+    suspend fun registerUser(
+        name: String,
+        email: String,
+        password: String,
+        onSuccessListener: () -> Unit,
+        onErrorListener: (exception: Exception) -> Unit,
+    )
+
+    suspend fun getCurrentUser() : Flow<FirebaseUser?>
+
+    suspend fun signIn(
+        email: String,
+        password: String,
+        onSuccessListener: () -> Unit,
+        onErrorListener: (exception: Exception) -> Unit,
+    )
+
+    fun getUserProfileData(
+        onSuccess: () -> Unit,
+        onFailure: (exception: Exception) -> Unit,
+    ): Flow<UserProfileData?>
+
+    suspend fun sendPasswordResetEmail(
+        email: String,
+        onSuccess: () -> Unit,
+        onFailure: (exception: Exception) -> Unit,
+    )
+
+    suspend fun updateUserProfile(
+        userProfileData: UserProfileData,
+        onSuccess: () -> Unit,
+        onFailure: (exception: Exception) -> Unit,
+    )
+
+    suspend fun createUserFirestoreData(
+        user: UserProfileData,
+        onSuccess: () -> Unit,
+        onFailure: (exception: Exception) -> Unit,
+    )
+
+    suspend fun signOut()
+
+    suspend fun deleteUserAndData(
+        email: String,
+        onSuccessListener: () -> Unit,
+        onErrorListener: (exception: Exception) -> Unit,
+    )
+}
