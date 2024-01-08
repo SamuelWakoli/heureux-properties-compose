5ea21a898de0eb606d5e98b757bb7d5fe819b5bf
Sam <swwakoli@gmail.com>
2024-01-08 10:59:28 +0300

    admin app theming

--- heureuxadmin/src/main/java/com/heureux/admin/ui/theme/Theme.kt
+++ heureuxadmin/src/main/java/com/heureux/admin/ui/theme/Theme.kt
@@ -15,33 +15,41 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+private val LightColorScheme = lightColorScheme(
+    primary = primaryColorLight,
+    onPrimary = onPrimaryColorLight,
+    primaryContainer = primaryContainerColorLight,
+    onPrimaryContainer = onPrimaryContainerColorLight,
+    secondary = secondaryColorLight,
+    onSecondary = onSecondaryColorLight,
+    secondaryContainer = secondaryContainerColorLight,
+    onSecondaryContainer = onSecondaryContainerColorLight,
+    tertiary = tertiaryColorLight,
+    onTertiary = onTertiaryColorLight,
+    tertiaryContainer = tertiaryContainerColorLight,
+    onTertiaryContainer = onTertiaryContainerColorLight
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val DarkColorScheme = darkColorScheme(
+    primary = primaryColorDark,
+    onPrimary = onPrimaryColorDark,
+    primaryContainer = primaryContainerColorDark,
+    onPrimaryContainer = onPrimaryContainerColorDark,
+    secondary = secondaryColorDark,
+    onSecondary = onSecondaryColorDark,
+    secondaryContainer = secondaryContainerColorDark,
+    onSecondaryContainer = onSecondaryContainerColorDark,
+    tertiary = tertiaryColorDark,
+    onTertiary = onTertiaryColorDark,
+    tertiaryContainer = tertiaryContainerColorDark,
+    onTertiaryContainer = onTertiaryContainerColorDark
 )
 
 @Composable
 fun HeureuxPropertiesTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
@@ -57,8 +65,8 @@ fun HeureuxPropertiesTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = colorScheme.surface.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
