43aa31e84230235540ac80740e653f5fe6705117
Sam <swwakoli@gmail.com>
2024-01-08 11:16:22 +0300

    inserted ProfileRepository

---
+++ heureuxadmin/src/main/java/com/heureux/admin/data/types/UserProfileData.kt
@@ -0,0 +1,12 @@
+package com.heureux.admin.data.types
+
+import android.net.Uri
+
+
+data class UserProfileData(
+    val userID: String,
+    val displayName: String?,
+    val photoURL: Uri?,
+    val userEmail: String?,
+    val phone: String? = null,
+)
