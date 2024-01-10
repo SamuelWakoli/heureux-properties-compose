24a978d4eb2acaf7269b7fbc1295c79f89907201
Sam <swwakoli@gmail.com>
2024-01-10 17:01:56 +0300

    InquiriesScreen body design

--- heureuxadmin/src/main/java/com/heureux/admin/ui/presentation/screens/main_screen/bottom_nav_destinations/inquiries_screen/InquiriesScreen.kt
+++ heureuxadmin/src/main/java/com/heureux/admin/ui/presentation/screens/main_screen/bottom_nav_destinations/inquiries_screen/InquiriesScreen.kt
@@ -1,11 +1,26 @@
 package com.heureux.admin.ui.presentation.screens.main_screen.bottom_nav_destinations.inquiries_screen
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Archive
+import androidx.compose.material.icons.outlined.ArrowForwardIos
+import androidx.compose.material.icons.outlined.Business
+import androidx.compose.material.icons.outlined.Cases
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
@@ -20,9 +35,108 @@ fun InquiriesScreen(
     viewModel: MainScreenViewModel,
 ) {
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .verticalScroll(rememberScrollState())
+            .fillMaxSize()
     ) {
-        Text(text = "Inquiries")
+        ListItem(
+            modifier = Modifier
+                .clickable { /*TODO*/ }
+                .widthIn(max = 600.dp),
+            leadingContent = {
+                Icon(imageVector = Icons.Outlined.Business, contentDescription = null)
+            },
+            headlineContent = {
+                Text(text = "Property inquiry", style = MaterialTheme.typography.titleMedium)
+            },
+            trailingContent = {
+                Icon(
+                    imageVector = Icons.Outlined.ArrowForwardIos,
+                    contentDescription = null,
+                    modifier = Modifier.size(16.dp)
+                )
+            },
+            colors = ListItemDefaults.colors(
+                leadingIconColor = MaterialTheme.colorScheme.primary,
+                headlineColor = MaterialTheme.colorScheme.primary,
+                trailingIconColor = MaterialTheme.colorScheme.primary,
+            )
+        )
+        ListItem(
+            modifier = Modifier
+                .clickable { /*TODO*/ }
+                .widthIn(max = 600.dp),
+            leadingContent = {
+                Icon(imageVector = Icons.Outlined.Cases, contentDescription = null)
+            },
+            headlineContent = {
+                Text(text = "Sell with us requests", style = MaterialTheme.typography.titleMedium)
+            },
+            trailingContent = {
+                Icon(
+                    imageVector = Icons.Outlined.ArrowForwardIos,
+                    contentDescription = null,
+                    modifier = Modifier.size(16.dp)
+                )
+            },
+            colors = ListItemDefaults.colors(
+                leadingIconColor = MaterialTheme.colorScheme.primary,
+                headlineColor = MaterialTheme.colorScheme.primary,
+                trailingIconColor = MaterialTheme.colorScheme.primary,
+            )
+        )
+        ListItem(
+            modifier = Modifier
+                .clickable { /*TODO*/ }
+                .widthIn(max = 600.dp),
+            leadingContent = {
+                Icon(imageVector = Icons.Outlined.Archive, contentDescription = null)
+            },
+            headlineContent = {
+                Text(
+                    text = "Archived property inquiry",
+                    style = MaterialTheme.typography.titleMedium
+                )
+            },
+            trailingContent = {
+                Icon(
+                    imageVector = Icons.Outlined.ArrowForwardIos,
+                    contentDescription = null,
+                    modifier = Modifier.size(16.dp)
+                )
+            },
+            colors = ListItemDefaults.colors(
+                leadingIconColor = MaterialTheme.colorScheme.primary,
+                headlineColor = MaterialTheme.colorScheme.primary,
+                trailingIconColor = MaterialTheme.colorScheme.primary,
+            )
+        )
+        ListItem(
+            modifier = Modifier
+                .clickable { /*TODO*/ }
+                .widthIn(max = 600.dp),
+            leadingContent = {
+                Icon(imageVector = Icons.Outlined.Archive, contentDescription = null)
+            },
+            headlineContent = {
+                Text(
+                    text = "Archived sell with us requests",
+                    style = MaterialTheme.typography.titleMedium
+                )
+            },
+            trailingContent = {
+                Icon(
+                    imageVector = Icons.Outlined.ArrowForwardIos,
+                    contentDescription = null,
+                    modifier = Modifier.size(16.dp)
+                )
+            },
+            colors = ListItemDefaults.colors(
+                leadingIconColor = MaterialTheme.colorScheme.primary,
+                headlineColor = MaterialTheme.colorScheme.primary,
+                trailingIconColor = MaterialTheme.colorScheme.primary,
+            )
+        )
     }
 }
 
