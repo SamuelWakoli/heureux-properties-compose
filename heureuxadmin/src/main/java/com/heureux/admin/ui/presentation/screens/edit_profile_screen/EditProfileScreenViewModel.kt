5b61f3d43b366140da8e46750f39d3fba416ebe6
Sam <swwakoli@gmail.com>
2024-01-12 01:42:01 +0300

    Added EditProfileScreenViewModel

---
+++ heureuxadmin/src/main/java/com/heureux/admin/ui/presentation/screens/edit_profile_screen/EditProfileScreenViewModel.kt
@@ -0,0 +1,153 @@
+package com.heureux.admin.ui.presentation.screens.edit_profile_screen
+
+import android.net.Uri
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.heureux.admin.data.FirebaseDirectories
+import com.heureux.admin.data.repositories.ProfileRepository
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+data class EditProfileScreenUiState(
+    val isLoadingUserData: Boolean = true,
+    val userName: String = "",
+    val userNameError: Boolean = false,
+    val phoneNumber: String = "",
+    val phoneNumberError: Boolean = false,
+    val photoURL: String = "",
+    val newPhotoURL: String = "",
+    val showBottomSheet: Boolean = false,
+    val isSaving: Boolean = false,
+)
+
+class EditProfileScreenViewModel(val profileRepository: ProfileRepository) : ViewModel() {
+
+    // by lazy so as to buy time for current user to be fetched / configured
+    // after authentication
+    val userProfileData by lazy {
+        profileRepository.getUserProfileData(
+            onSuccess = {},
+            onFailure = {}
+        ).stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000L),
+            initialValue = null
+        )
+    }
+
+    private val _uiState: MutableStateFlow<EditProfileScreenUiState> by lazy {
+        MutableStateFlow(
+            EditProfileScreenUiState(
+                userName = "",
+                phoneNumber = "",
+            )
+        )
+    }
+
+    init {
+        /** after 4 seconds (estimated time), the [userProfileData] should have been updated
+         * so we get to update the [_uiState] with latest data
+         */
+        viewModelScope.launch {
+            delay(4_000L)
+        }.invokeOnCompletion {
+            _uiState.update {
+                it.copy(
+                    photoURL = userProfileData.value?.photoURL.toString(),
+                    userName = userProfileData.value?.displayName ?: "",
+                    phoneNumber = userProfileData.value?.phone ?: "",
+                    isLoadingUserData = false,
+                )
+            }
+        }
+    }
+
+    val uiState: StateFlow<EditProfileScreenUiState> by lazy { _uiState.asStateFlow() }
+
+    fun uploadProfileImage(
+        uri: Uri,
+        onSuccess: (downloadUrl: String) -> Unit,
+        onFailure: (exception: Exception) -> Unit,
+    ) = viewModelScope.launch {
+        profileRepository.uploadImageGetUrl(
+            uri = uri,
+            directory = "${FirebaseDirectories.UsersStorageReference.name}/${userProfileData.value?.userEmail ?: Firebase.auth.currentUser?.email!!}/profile image.png",
+            onSuccessListener = onSuccess,
+            onFailure = onFailure,
+        )
+    }
+
+    fun updatePhotoURL(photoURL: String) {
+        _uiState.update { it.copy(newPhotoURL = photoURL) }
+    }
+
+    fun updateUserName(userName: String) =
+        _uiState.update { it.copy(userName = userName) }
+
+    fun updatePhoneNumber(phoneNumber: String) =
+        _uiState.update { it.copy(phoneNumber = phoneNumber) }
+
+    fun hideOrShowEditProfileBottomSheet() =
+        _uiState.update { it.copy(showBottomSheet = !it.showBottomSheet) }
+
+    fun updateProfile(onSuccess: () -> Unit, onFailure: (exception: Exception) -> Unit) {
+        if (uiState.value.userName.isEmpty()) {
+            _uiState.update {
+                it.copy(
+                    userNameError = true
+                )
+            }
+        } else if (uiState.value.phoneNumber.isEmpty()) {
+            _uiState.update {
+                it.copy(
+                    phoneNumberError = true
+                )
+            }
+        } else {
+            _uiState.update {
+                it.copy(
+                    userNameError = false,
+                    phoneNumberError = false,
+                    isSaving = true
+                )
+            }
+
+
+
+            viewModelScope.launch {
+                profileRepository.updateUserProfile(
+                    userProfileData = userProfileData.value!!.copy(
+                        displayName = uiState.value.userName,
+                        phone = uiState.value.phoneNumber,
+                        photoURL = Uri.parse(uiState.value.newPhotoURL),
+                    ),
+                    onSuccess = {
+                        onSuccess()
+                    },
+                    onFailure = { exception: Exception ->
+                        onFailure(exception)
+                        _uiState.update {
+                            it.copy(
+                                isSaving = false
+                            )
+                        }
+                    }
+                )
+            }.invokeOnCompletion {
+                _uiState.update {
+                    it.copy(
+                        isSaving = false
+                    )
+                }
+            }
+        }
+    }
+}
