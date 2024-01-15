3511d4535db58eff7dea5d0747eea5e30aacca44
Sam <swwakoli@gmail.com>
2024-01-15 11:13:53 +0300

    Implemented function to get all users

---
+++ heureuxadmin/src/main/java/com/heureux/admin/data/sources/HeureuxUsersDataSource.kt
@@ -0,0 +1,40 @@
+package com.heureux.admin.data.sources
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
+import com.heureux.admin.data.FirebaseDirectories
+import com.heureux.admin.data.types.HeureuxUser
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+
+class HeureuxUsersDataSource : UsersDataSource {
+    override val firestore: FirebaseFirestore
+        get() = Firebase.firestore
+    override val storage: FirebaseStorage
+        get() = Firebase.storage
+
+    override fun getAllUsers(
+        onFailure: (exception: Exception) -> Unit
+    ): Flow<List<HeureuxUser>> = callbackFlow {
+        val snapshotListener = firestore.collection(FirebaseDirectories.UsersCollection.name)
+            .addSnapshotListener { snapshot, error ->
+                if (error != null) {
+                    close(error)
+                    onFailure(error)
+                } else {
+                    val users = mutableListOf<HeureuxUser>()
+                    for (document in snapshot!!.documents) {
+                        val user = document.toObject(HeureuxUser::class.java)
+                        user?.let { users.add(it) }
+                    }
+                    trySend(users)
+                }
+            }
+
+        awaitClose { snapshotListener.remove() }
+    }
+}
---
+++ heureuxadmin/src/main/java/com/heureux/admin/data/sources/UsersDataSource.kt
@@ -0,0 +1,15 @@
+package com.heureux.admin.data.sources
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.heureux.admin.data.types.HeureuxUser
+import kotlinx.coroutines.flow.Flow
+
+interface UsersDataSource {
+    val firestore: FirebaseFirestore
+    val storage: FirebaseStorage
+
+    fun getAllUsers(
+        onFailure: (exception: Exception) -> Unit
+    ) : Flow<List<HeureuxUser>>
+}
