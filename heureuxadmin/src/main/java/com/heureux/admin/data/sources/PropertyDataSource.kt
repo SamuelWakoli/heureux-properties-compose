1bc1f40258ba1755221e4fe0975ab60439a8c710
Sam <swwakoli@gmail.com>
2024-01-15 10:34:20 +0300

    Created PropertyDataSource interface for the admin app

---
+++ heureuxadmin/src/main/java/com/heureux/admin/data/sources/PropertyDataSource.kt
@@ -0,0 +1,32 @@
+package com.heureux.admin.data.sources
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.heureux.admin.data.types.HeureuxProperty
+import kotlinx.coroutines.flow.Flow
+
+interface PropertyDataSource {
+    val firestore: FirebaseFirestore
+    val storage: FirebaseStorage
+
+    fun getPropertyData(id: String) : Flow<HeureuxProperty>
+    fun getProperties() : Flow<List<HeureuxProperty>>
+
+    suspend fun addProperty(
+        onSuccess: () -> Unit,
+        onFailure: (exception: Exception) -> Unit,
+        data: HeureuxProperty
+    )
+    suspend fun updateProperty(
+        onSuccess: () -> Unit,
+        onFailure: (exception: Exception) -> Unit,
+        data: HeureuxProperty
+    )
+
+    suspend fun deleteProperty(
+        onSuccess: () -> Unit,
+        onFailure: (exception: Exception) -> Unit,
+        id: String
+    )
+
+}
