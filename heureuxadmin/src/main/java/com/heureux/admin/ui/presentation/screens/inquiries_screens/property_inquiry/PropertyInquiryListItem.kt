3359b4617d88d8c6e0cb745609eea601b22277ac
Sam <swwakoli@gmail.com>
2024-01-17 09:58:48 +0300

    Refined PropertyInquiryListItem

--- heureuxadmin/src/main/java/com/heureux/admin/ui/presentation/screens/inquiries_screens/property_inquiry/PropertyInquiryListItem.kt
+++ heureuxadmin/src/main/java/com/heureux/admin/ui/presentation/screens/inquiries_screens/property_inquiry/PropertyInquiryListItem.kt
@@ -50,186 +50,209 @@ fun PropertyInquiryListItem(
     var showOptions by remember { mutableStateOf(false) }
     var showApproveDialog by remember { mutableStateOf(false) }
 
-    val allProperties = viewModel.allProperties.collectAsState().value
-    val allUsers = viewModel.allUsers.collectAsState().value
     val context = LocalContext.current
 
-    ListItem(modifier = Modifier.widthIn(max = 600.dp), overlineContent = {
-        Text(
-            text = "12:30pm  11/02/2024",
-            color = MaterialTheme.colorScheme.secondary,
-        )
-    }, headlineContent = {
-        Text(
-            text = "Property Name",
-            color = MaterialTheme.colorScheme.primary,
-        )
-    }, supportingContent = {
-        Text(text = buildAnnotatedString {
-            append("Username: ")
-            withStyle(SpanStyle(color = MaterialTheme.colorScheme.primary)) {
-                append("John Doe")
-            }
-        })
-
-        Text(text = buildAnnotatedString {
-            append("Property price: ")
-            withStyle(SpanStyle(color = MaterialTheme.colorScheme.primary)) {
-                append("2,000,000")
-            }
-        })
-
-        Text(text = buildAnnotatedString {
-            append("Offer amount: ")
-            withStyle(SpanStyle(color = MaterialTheme.colorScheme.primary)) {
-                append("1,800,000")
-            }
-        })
-
-        Text(text = buildAnnotatedString {
-            append("Preferred payment method: ")
-            withStyle(SpanStyle(color = MaterialTheme.colorScheme.primary)) {
-                append("Mortgage")
-            }
-        })
-    }, trailingContent = {
-        IconButton(onClick = { showOptions = true }) {
-            Icon(
-                imageVector = if (showOptions) Icons.Outlined.Close else Icons.Outlined.MoreVert,
-                contentDescription = if (showOptions) "Hide" else "Options"
+    val currentProperty =
+        viewModel.allProperties.collectAsState().value?.find { it.id == inquiryItem.propertyId }
+    val currentUser =
+        viewModel.allUsers.collectAsState().value?.find { it.email == inquiryItem.senderId }
+    if (currentUser != null && currentProperty != null) {
+        ListItem(modifier = Modifier.widthIn(max = 600.dp), overlineContent = {
+            val time = LocalDateTime.parse(inquiryItem.time)
+            Text(
+                text = "Date: ${time.dayOfMonth}/${time.monthValue}/${time.year}      Times: ${time.hour}:${time.minute}",
+                color = MaterialTheme.colorScheme.secondary,
             )
-        }
-
-        DropdownMenu(expanded = showOptions, onDismissRequest = { showOptions = false }) {
-            DropdownMenuItem(leadingIcon = {
-                Icon(
-                    imageVector = Icons.Outlined.DoneAll, contentDescription = null
-                )
-            }, text = { Text(text = "Approve") }, onClick = {
-                showOptions = false
-                showApproveDialog = true
+        }, headlineContent = {
+            Text(
+                text = currentProperty.name,
+                color = MaterialTheme.colorScheme.primary,
+            )
+        }, supportingContent = {
+            Text(text = buildAnnotatedString {
+                append("Username: ")
+                withStyle(SpanStyle(color = MaterialTheme.colorScheme.primary)) {
+                    append(currentUser.name ?: "")
+                }
             })
-            DropdownMenuItem(leadingIcon = {
+
+            Text(text = buildAnnotatedString {
+                append("Property price: ")
+                withStyle(SpanStyle(color = MaterialTheme.colorScheme.primary)) {
+                    append("Ksh. ${currentProperty.price}")
+                }
+            })
+
+            Text(text = buildAnnotatedString {
+                append("Offer amount: ")
+                withStyle(SpanStyle(color = MaterialTheme.colorScheme.primary)) {
+                    append("Ksh. ${inquiryItem.offerAmount}")
+                }
+            })
+
+            Text(text = buildAnnotatedString {
+                append("Preferred payment method: ")
+                withStyle(SpanStyle(color = MaterialTheme.colorScheme.primary)) {
+                    append(inquiryItem.preferredPaymentMethod)
+                }
+            })
+        }, trailingContent = {
+            IconButton(onClick = { showOptions = true }) {
                 Icon(
-                    imageVector = Icons.Outlined.Call, contentDescription = null
+                    imageVector = if (showOptions) Icons.Outlined.Close else Icons.Outlined.MoreVert,
+                    contentDescription = if (showOptions) "Hide" else "Options"
                 )
-            },
-                text = { Text(text = "Call ${allUsers?.find { it.email == inquiryItem.senderId }?.name}") },
-                onClick = {
+            }
+
+            DropdownMenu(expanded = showOptions, onDismissRequest = { showOptions = false }) {
+                DropdownMenuItem(leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Outlined.DoneAll, contentDescription = null
+                    )
+                }, text = { Text(text = "Approve") }, onClick = {
                     showOptions = false
-                    val phone = allUsers?.find { it.email == inquiryItem.senderId }?.phone
-                    if (phone != null && phone != "null") {
-                        val intent = Intent(Intent.ACTION_DIAL).apply {
-                            data = Uri.parse("tel:$phone")
-                        }
-                        try {
-                            context.startActivity(intent)
-                        } catch (e: ActivityNotFoundException) {
+                    showApproveDialog = true
+                })
+                DropdownMenuItem(leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Outlined.Call, contentDescription = null
+                    )
+                },
+                    text = { Text(text = "Call ${currentUser.name}") },
+                    onClick = {
+                        showOptions = false
+                        val phone = currentUser.phone
+                        if (phone != null && phone != "null") {
+                            val intent = Intent(Intent.ACTION_DIAL).apply {
+                                data = Uri.parse("tel:$phone")
+                            }
+                            try {
+                                context.startActivity(intent)
+                            } catch (e: ActivityNotFoundException) {
+                                Toast.makeText(
+                                    context, "No phone app found", Toast.LENGTH_LONG
+                                ).show()
+                            }
+
+                        } else {
                             Toast.makeText(
-                                context, "No phone app found", Toast.LENGTH_LONG
+                                context,
+                                "This user has not added phone number",
+                                Toast.LENGTH_SHORT
                             ).show()
                         }
 
-                    } else {
+                    })
+
+                if (!inquiryItem.archived) DropdownMenuItem(leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Outlined.Archive, contentDescription = null
+                    )
+                }, text = { Text(text = "Archive") }, onClick = {
+                    showOptions = false
+                    viewModel.updateArchivePropertyInquiry(
+                        inquiryItem,
+                        onSuccess = {
+                            Toast.makeText(
+                                context, "Successfully archived inquiry", Toast.LENGTH_SHORT
+                            ).show()
+                        },
+                        onFailure = {
+                            Toast.makeText(
+                                context, "Failed to archive inquiry", Toast.LENGTH_LONG
+                            ).show()
+                        },
+                    )
+                })
+                Divider(
+                    modifier = Modifier.padding(4.dp)
+                )
+                DropdownMenuItem(leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Outlined.Delete, contentDescription = null
+                    )
+                }, text = { Text(text = "Delete") }, colors = MenuDefaults.itemColors(
+                    leadingIconColor = MaterialTheme.colorScheme.error,
+                    textColor = MaterialTheme.colorScheme.error,
+                ), onClick = {
+                    showOptions = false
+                    viewModel.deletePropertyInquiry(inquiryItem, onSuccess = {
                         Toast.makeText(
-                            context, "This user has not added phone number", Toast.LENGTH_SHORT
+                            context, "Successfully deleted inquiry", Toast.LENGTH_SHORT
                         ).show()
-                    }
+                    }, onFailure = {
+                        Toast.makeText(
+                            context, "Failed to delete inquiry", Toast.LENGTH_LONG
+                        ).show()
+                    })
+                })
+            }
+        })
+
+
+        if (showApproveDialog) {
+            PropertyApprovalDialog(propertyName = currentProperty.name,
+                buyerName = currentUser.name ?: "",
+                price = inquiryItem.offerAmount,
+                onDismissRequest = { showApproveDialog = false },
+                onApprove = {
+                    viewModel.updateProperty(
+                        currentProperty.copy(
+                            purchasedBy = inquiryItem.senderId
+                        ),
+                        onSuccess = {
+                            viewModel.addPayment(
+                                payment = PaymentItem(
+                                    paymentId = LocalDateTime.now().toString(),
+                                    propertyId = inquiryItem.propertyId,
+                                    userId = inquiryItem.senderId,
+                                    amount = "0",
+                                    agreedPrice = inquiryItem.offerAmount,
+                                    totalAmountPaid = "0",
+                                    owingAmount = inquiryItem.offerAmount,
+                                    paymentMethod = inquiryItem.preferredPaymentMethod,
+                                    time = LocalDateTime.now().toString(),
+                                    approvedBy = Firebase.auth.currentUser?.email ?: "",
+                                ),
+                                onSuccess = {
+                                    viewModel.deletePropertyInquiry(
+                                        inquiryItem,
+                                        onSuccess = {
+                                            Toast.makeText(
+                                                context,
+                                                "Successfully deleted inquiry",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+                                        },
+                                        onFailure = {
+                                            Toast.makeText(
+                                                context,
+                                                "Failed to delete this approved inquiry",
+                                                Toast.LENGTH_LONG
+                                            ).show()
+                                        }
+                                    )
+                                    Toast.makeText(
+                                        context,
+                                        "Successfully approved inquiry",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                },
+                                onFailure = {
+                                    Toast.makeText(
+                                        context, "Failed to approve inquiry", Toast.LENGTH_LONG
+                                    ).show()
+                                }
+                            )
+                        },
+                        onFailure = {
+                            Toast.makeText(
+                                context, "Failed to update property", Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    )
 
                 })
-
-            if (!inquiryItem.archived) DropdownMenuItem(leadingIcon = {
-                Icon(
-                    imageVector = Icons.Outlined.Archive, contentDescription = null
-                )
-            }, text = { Text(text = "Archive") }, onClick = {
-                showOptions = false
-                viewModel.updateArchivePropertyInquiry(
-                    inquiryItem,
-                    onSuccess = {
-                        Toast.makeText(
-                            context, "Successfully archived inquiry", Toast.LENGTH_SHORT
-                        ).show()
-                    },
-                    onFailure = {
-                        Toast.makeText(
-                            context, "Failed to archive inquiry", Toast.LENGTH_LONG
-                        ).show()
-                    },
-                )
-            })
-            Divider(
-                modifier = Modifier.padding(4.dp)
-            )
-            DropdownMenuItem(leadingIcon = {
-                Icon(
-                    imageVector = Icons.Outlined.Delete, contentDescription = null
-                )
-            }, text = { Text(text = "Delete") }, colors = MenuDefaults.itemColors(
-                leadingIconColor = MaterialTheme.colorScheme.error,
-                textColor = MaterialTheme.colorScheme.error,
-            ), onClick = {
-                showOptions = false
-                viewModel.deletePropertyInquiry(inquiryItem, onSuccess = {
-                    Toast.makeText(
-                        context, "Successfully deleted inquiry", Toast.LENGTH_SHORT
-                    ).show()
-                }, onFailure = {
-                    Toast.makeText(
-                        context, "Failed to delete inquiry", Toast.LENGTH_LONG
-                    ).show()
-                })
-            })
         }
-    })
-
-
-    if (showApproveDialog) {
-        PropertyApprovalDialog(propertyName = allProperties?.find { it.id == inquiryItem.propertyId }?.name
-            ?: "",
-            buyerName = allUsers?.find { it.email == inquiryItem.senderId }?.name ?: "",
-            price = inquiryItem.offerAmount,
-            onDismissRequest = { showApproveDialog = false },
-            onApprove = {
-                viewModel.addPayment(
-                    payment = PaymentItem(
-                        paymentId = LocalDateTime.now().toString(),
-                        propertyId = inquiryItem.propertyId,
-                        userId = inquiryItem.senderId,
-                        amount = "0",
-                        agreedPrice = inquiryItem.offerAmount,
-                        totalAmountPaid = "0",
-                        owingAmount = inquiryItem.offerAmount,
-                        paymentMethod = inquiryItem.preferredPaymentMethod,
-                        time = LocalDateTime.now().toString(),
-                        approvedBy = Firebase.auth.currentUser?.email ?: "",
-                    ),
-                    onSuccess = {
-                        viewModel.deletePropertyInquiry(
-                            inquiryItem,
-                            onSuccess = {
-                                Toast.makeText(
-                                    context, "Successfully deleted inquiry", Toast.LENGTH_SHORT
-                                ).show()
-                            },
-                            onFailure = {
-                                Toast.makeText(
-                                    context,
-                                    "Failed to delete this approved inquiry",
-                                    Toast.LENGTH_LONG
-                                ).show()
-                            }
-                        )
-                        Toast.makeText(
-                            context, "Successfully approved inquiry", Toast.LENGTH_SHORT
-                        ).show()
-                    },
-                    onFailure = {
-                        Toast.makeText(
-                            context, "Failed to approve inquiry", Toast.LENGTH_LONG
-                        ).show()
-                    }
-                )
-            })
     }
 }
