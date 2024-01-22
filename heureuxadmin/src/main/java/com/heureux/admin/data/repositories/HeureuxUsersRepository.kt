c9b36006bad1d93b43f1da9f5ce520971ec99be2
Sam <swwakoli@gmail.com>
2024-01-22 14:06:14 +0300

    Documentation for UsersRepository

--- heureuxadmin/src/main/java/com/heureux/admin/data/repositories/HeureuxUsersRepository.kt
+++ heureuxadmin/src/main/java/com/heureux/admin/data/repositories/HeureuxUsersRepository.kt
@@ -5,19 +5,50 @@ import com.heureux.admin.data.types.FeedbackItem
 import com.heureux.admin.data.types.HeureuxUser
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * A repository that provides access to Heureux user data.
+ *
+ * @param dataSource The data source to use for accessing user data.
+ */
 class HeureuxUsersRepository(private val dataSource: UsersDataSource) : UsersRepository {
+    /**
+     * Gets all Heureux users.
+     *
+     * @param onFailure A callback to be invoked if an exception occurs.
+     * @return A flow of lists of Heureux users.
+     */
     override fun getAllUsers(onFailure: (exception: Exception) -> Unit): Flow<List<HeureuxUser>> =
         dataSource.getAllUsers(onFailure)
 
+    /**
+     * Gets all feedback items.
+     *
+     * @param onFailure A callback to be invoked if an exception occurs.
+     * @return A flow of lists of feedback items.
+     */
     override fun getFeedback(onFailure: (exception: Exception) -> Unit): Flow<List<FeedbackItem>> =
         dataSource.getFeedback(onFailure)
 
+    /**
+     * Updates a feedback item.
+     *
+     * @param feedbackItem The feedback item to update.
+     * @param onSuccess A callback to be invoked if the update is successful.
+     * @param onFailure A callback to be invoked if an exception occurs.
+     */
     override suspend fun updateFeedback(
         feedbackItem: FeedbackItem,
         onSuccess: () -> Unit,
         onFailure: (exception: Exception) -> Unit
     ) = dataSource.updateFeedback(feedbackItem, onSuccess, onFailure)
 
+    /**
+     * Deletes a feedback item.
+     *
+     * @param feedbackItem The feedback item to delete.
+     * @param onSuccess A callback to be invoked if the deletion is successful.
+     * @param onFailure A callback to be invoked if an exception occurs.
+     */
     override suspend fun deleteFeedback(
         feedbackItem: FeedbackItem,
         onSuccess: () -> Unit,
--- heureuxadmin/src/main/java/com/heureux/admin/data/repositories/UsersRepository.kt
+++ heureuxadmin/src/main/java/com/heureux/admin/data/repositories/UsersRepository.kt
@@ -4,22 +4,51 @@ import com.heureux.admin.data.types.FeedbackItem
 import com.heureux.admin.data.types.HeureuxUser
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * An interface for interacting with a user repository.
+ */
 interface UsersRepository {
 
+    /**
+     * Gets all users.
+     *
+     * @param onFailure A callback to be invoked if an exception occurs.
+     * @return A Flow of a list of HeureuxUser objects.
+     */
     fun getAllUsers(
         onFailure: (exception: Exception) -> Unit
     ) : Flow<List<HeureuxUser>>
 
+    /**
+     * Gets feedback.
+     *
+     * @param onFailure A callback to be invoked if an exception occurs.
+     * @return A Flow of a list of FeedbackItem objects.
+     */
     fun getFeedback(
         onFailure: (exception: Exception) -> Unit
     ): Flow<List<FeedbackItem>>
 
+    /**
+     * Updates a feedback item.
+     *
+     * @param feedbackItem The feedback item to update.
+     * @param onSuccess A callback to be invoked if the update is successful.
+     * @param onFailure A callback to be invoked if an exception occurs.
+     */
     suspend fun updateFeedback(
         feedbackItem: FeedbackItem,
         onSuccess: () -> Unit,
         onFailure: (exception: Exception) -> Unit
     )
 
+    /**
+     * Deletes a feedback item.
+     *
+     * @param feedbackItem The feedback item to delete.
+     * @param onSuccess A callback to be invoked if the deletion is successful.
+     * @param onFailure A callback to be invoked if an exception occurs.
+     */
     suspend fun deleteFeedback(
         feedbackItem: FeedbackItem,
         onSuccess: () -> Unit,
