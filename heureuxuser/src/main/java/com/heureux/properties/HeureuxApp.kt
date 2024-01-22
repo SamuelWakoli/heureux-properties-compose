677b079442d841406105a1fc5269d891c08cda68
Sam <swwakoli@gmail.com>
2024-01-22 15:20:57 +0300

    User app documentation finalized

--- heureuxuser/src/main/java/com/heureux/properties/HeureuxApp.kt
+++ heureuxuser/src/main/java/com/heureux/properties/HeureuxApp.kt
@@ -7,15 +7,35 @@ import com.heureux.properties.data.AppContainer
 import com.heureux.properties.data.HeureuxAppContainer
 import com.heureux.properties.data.repositories.UserPreferencesRepository
 
+/**
+ * The name of the user preferences file.
+ */
 private const val USER_PREFERENCE_NAME = "user_preferences"
+
+/**
+ * Creates a Preferences DataStore for user preferences.
+ */
 val Context.user_dataStore by preferencesDataStore(
     name = USER_PREFERENCE_NAME
 )
 
+/**
+ * The main application class.
+ */
 class HeureuxApp : Application() {
+    /**
+     * The application container.
+     */
     lateinit var container: AppContainer
+
+    /**
+     * The user preferences repository.
+     */
     lateinit var userPreferencesRepository: UserPreferencesRepository
 
+    /**
+     * Called when the application is created.
+     */
     override fun onCreate() {
         super.onCreate()
 
--- heureuxuser/src/main/java/com/heureux/properties/data/AppContainer.kt
+++ heureuxuser/src/main/java/com/heureux/properties/data/AppContainer.kt
@@ -8,11 +8,17 @@ import com.heureux.properties.data.repositories.PropertiesRepository
 import com.heureux.properties.data.sources.HeureuxProfileDataSource
 import com.heureux.properties.data.sources.HeureuxPropertiesDataSource
 
+/**
+ * An interface that provides access to the profile and properties repositories.
+ */
 interface AppContainer {
     val profileRepository: ProfileRepository
     val propertiesRepository: PropertiesRepository
 }
 
+/**
+ * An implementation of the AppContainer interface that uses the Heureux data sources.
+ */
 class HeureuxAppContainer(context: Context) : AppContainer {
 
     override val profileRepository: ProfileRepository =
