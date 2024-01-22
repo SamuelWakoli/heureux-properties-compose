41f729fec97300fb05dd54a107d83c903e680641
Sam <swwakoli@gmail.com>
2024-01-22 13:01:44 +0300

    Added button to view image on CoilImage

--- heureuxuser/src/main/java/com/heureux/properties/ui/presentation/composables/images/CoilImage.kt
+++ heureuxuser/src/main/java/com/heureux/properties/ui/presentation/composables/images/CoilImage.kt
@@ -1,13 +1,18 @@
 package com.heureux.properties.ui.presentation.composables.images
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Error
+import androidx.compose.material.icons.outlined.Expand
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -18,21 +23,26 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.net.toUri
 import coil.compose.AsyncImagePainter
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import coil.transform.CircleCropTransformation
 import coil.transform.RoundedCornersTransformation
+import com.heureux.properties.utils.openImage
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CoilImage(
     modifier: Modifier = Modifier,
     imageUrl: String = "https://picsum.photos/200",
     applyCircleShape: Boolean = false,
+    showOpenImageButton: Boolean = true,
     errorContent: @Composable (() -> Unit)? = null,
     loadingContent: @Composable (() -> Unit)? = null,
     emptyContent: @Composable (() -> Unit)? = null,
 ) {
+    val context = LocalContext.current
     Box(
         modifier = modifier,
         contentAlignment = Alignment.Center,
@@ -106,6 +116,26 @@ fun CoilImage(
             contentScale = ContentScale.Crop,
             modifier = Modifier.fillMaxSize()
         )
+
+        if (showOpenImageButton)
+            Card(onClick = {
+                openImage(
+                    context = context,
+                    uri = imageUrl.toUri(),
+                    onError = {
+                        Toast.makeText(context, it.message, Toast.LENGTH_LONG).show()
+                    }
+                )
+            }, modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(4.dp)) {
+                Icon(
+                    imageVector = Icons.Outlined.Expand,
+                    contentDescription = "Expand image",
+                    tint = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.padding(8.dp)
+                )
+            }
 
     }
 }
