4717a52fcd48d6a6d72411822532749e1609eb07
Sam <swwakoli@gmail.com>
2024-01-22 13:34:58 +0300

    Added a utility to delete image from uri

--- heureuxadmin/src/main/java/com/heureux/admin/utils/DeleteStorageImage.kt
+++ heureuxadmin/src/main/java/com/heureux/admin/utils/DeleteStorageImage.kt
@@ -1,2 +1,26 @@
 package com.heureux.admin.utils
 
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+
+/**
+ * Deletes an image from the given URI.
+ *
+ * @param uri The URI of the image to delete.
+ * @param onSuccess A callback function that will be called if the image is deleted successfully.
+ * @param onFailure A callback function that will be called if the image deletion fails.
+ */
+fun deleteImageFromUri(
+    uri: String,
+    onSuccess: () -> Unit = {},
+    onFailure: (Exception) -> Unit = {}
+) {
+    val storage = Firebase.storage
+    val imageRef = storage.getReferenceFromUrl(uri)
+
+    imageRef.delete().addOnSuccessListener {
+        onSuccess()
+    }.addOnFailureListener {
+        onFailure(it)
+    }
+}
