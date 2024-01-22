53f2e690734b166c7085e9a6966b25924e2ade65
Sam <swwakoli@gmail.com>
2024-01-22 14:02:29 +0300

    Documentation for PropertyRepository

--- heureuxadmin/src/main/java/com/heureux/admin/data/repositories/HeureuxPropertyRepository.kt
+++ heureuxadmin/src/main/java/com/heureux/admin/data/repositories/HeureuxPropertyRepository.kt
@@ -5,7 +5,20 @@ import com.heureux.admin.data.sources.PropertyDataSource
 import com.heureux.admin.data.types.HeureuxProperty
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * A repository for accessing property data.
+ *
+ * @param dataSource The data source to use for accessing property data.
+ */
 class HeureuxPropertyRepository(private val dataSource: PropertyDataSource) : PropertyRepository {
+    /**
+     * Uploads an image and gets the download URL.
+     *
+     * @param uri The URI of the image to upload.
+     * @param directory The directory to upload the image to.
+     * @param onSuccessListener The listener to call when the upload is successful.
+     * @param onFailure The listener to call when the upload fails.
+     */
     override suspend fun uploadImageGetUrl(
         uri: Uri,
         directory: String,
@@ -13,21 +26,55 @@ class HeureuxPropertyRepository(private val dataSource: PropertyDataSource) : Pr
         onFailure: (exception: Exception) -> Unit
     ) = dataSource.uploadImageGetUrl(uri, directory, onSuccessListener, onFailure)
 
+    /**
+     * Gets the property data for the given ID.
+     *
+     * @param id The ID of the property to get.
+     * @param onFailure The listener to call when the operation fails.
+     * @return A Flow that emits the property data.
+     */
     override fun getPropertyData(
         id: String, onFailure: (exception: Exception) -> Unit
     ): Flow<HeureuxProperty> = dataSource.getPropertyData(id, onFailure)
 
+    /**
+     * Gets all properties.
+     *
+     * @param onFailure The listener to call when the operation fails.
+     * @return A Flow that emits a list of all properties.
+     */
     override fun getAllProperties(onFailure: (exception: Exception) -> Unit): Flow<List<HeureuxProperty>> =
         dataSource.getAllProperties(onFailure)
 
+    /**
+     * Adds a new property.
+     *
+     * @param onSuccess The listener to call when the operation is successful.
+     * @param onFailure The listener to call when the operation fails.
+     * @param data The property data to add.
+     */
     override suspend fun addProperty(
         onSuccess: () -> Unit, onFailure: (exception: Exception) -> Unit, data: HeureuxProperty
     ) = dataSource.addProperty(onSuccess, onFailure, data)
 
+    /**
+     * Updates an existing property.
+     *
+     * @param onSuccess The listener to call when the operation is successful.
+     * @param onFailure The listener to call when the operation fails.
+     * @param data The property data to update.
+     */
     override suspend fun updateProperty(
         onSuccess: () -> Unit, onFailure: (exception: Exception) -> Unit, data: HeureuxProperty
     ) = dataSource.updateProperty(onSuccess, onFailure, data)
 
+    /**
+     * Deletes a property.
+     *
+     * @param onSuccess The listener to call when the operation is successful.
+     * @param onFailure The listener to call when the operation fails.
+     * @param id The ID of the property to delete.
+     */
     override suspend fun deleteProperty(
         onSuccess: () -> Unit, onFailure: (exception: Exception) -> Unit, id: String
     ) = dataSource.deleteProperty(onSuccess, onFailure, id)
--- heureuxadmin/src/main/java/com/heureux/admin/data/repositories/PropertyRepository.kt
+++ heureuxadmin/src/main/java/com/heureux/admin/data/repositories/PropertyRepository.kt
@@ -4,8 +4,19 @@ import android.net.Uri
 import com.heureux.admin.data.types.HeureuxProperty
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * An interface for interacting with a property repository.
+ */
 interface PropertyRepository {
 
+    /**
+     * Uploads an image to the repository and returns its download URL.
+     *
+     * @param uri The URI of the image to upload.
+     * @param directory The directory in the repository to upload the image to.
+     * @param onSuccessListener A callback function that will be invoked with the download URL of the uploaded image.
+     * @param onFailure A callback function that will be invoked if an error occurs while uploading the image.
+     */
     suspend fun uploadImageGetUrl(
         uri: Uri,
         directory: String,
@@ -13,25 +24,59 @@ interface PropertyRepository {
         onFailure: (exception: Exception) -> Unit
     )
 
+    /**
+     * Gets property data for the given property ID.
+     *
+     * @param id The ID of the property to get data for.
+     * @param onFailure A callback function that will be invoked if an error occurs while getting the property data.
+     * @return A Flow that emits the property data.
+     */
     fun getPropertyData(
         id: String,
         onFailure: (exception: Exception) -> Unit
     ): Flow<HeureuxProperty>
 
+    /**
+     * Gets a list of all properties.
+     *
+     * @param onFailure A callback function that will be invoked if an error occurs while getting the list of properties.
+     * @return A Flow that emits the list of properties.
+     */
     fun getAllProperties(onFailure: (exception: Exception) -> Unit): Flow<List<HeureuxProperty>>
 
+    /**
+     * Adds a new property to the repository.
+     *
+     * @param onSuccess A callback function that will be invoked if the property is added successfully.
+     * @param onFailure A callback function that will be invoked if an error occurs while adding the property.
+     * @param data The property data to add.
+     */
     suspend fun addProperty(
         onSuccess: () -> Unit,
         onFailure: (exception: Exception) -> Unit,
         data: HeureuxProperty
     )
 
+    /**
+     * Updates an existing property in the repository.
+     *
+     * @param onSuccess A callback function that will be invoked if the property is updated successfully.
+     * @param onFailure A callback function that will be invoked if an error occurs while updating the property.
+     * @param data The property data to update.
+     */
     suspend fun updateProperty(
         onSuccess: () -> Unit,
         onFailure: (exception: Exception) -> Unit,
         data: HeureuxProperty
     )
 
+    /**
+     * Deletes a property from the repository.
+     *
+     * @param onSuccess A callback function that will be invoked if the property is deleted successfully.
+     * @param onFailure A callback function that will be invoked if an error occurs while deleting the property.
+     * @param id The ID of the property to delete.
+     */
     suspend fun deleteProperty(
         onSuccess: () -> Unit,
         onFailure: (exception: Exception) -> Unit,
